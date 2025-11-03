32defa5774671fd7139ab71e184dd1f07dd0cbd6
Saddam <saddamhussain1919@gmail.com>
2025-11-04 01:25:05 +0200

    added

--- angul-it/src/app/home/home.ts
+++ angul-it/src/app/home/home.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  standalone: true,  // Add this!
+  standalone: true,  
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {  // Rename from "Home" to "HomeComponent"
+export class HomeComponent { 
 
 }
